b5194fd5d8dea15ed7e184fd54f85602171741b2
Annique Nsabimana <anniquensabimana@gmail.com>
2019-09-04 16:18:30 -0500

    mvp met

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
+const usersRouter = require('./users/userRouter');
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
-});
+
 
 //custom middleware
 
+// logger should be applied to all requests
 function logger(req, res, next) {
-
+  console.log(req.method, req.originalUrl, new Date())
+  next();
 };
+
+server.get('/', logger, (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`)
+});
+
+server.use('/users', logger, usersRouter)
 
 module.exports = server;
